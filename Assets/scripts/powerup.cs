8ab81095557f0ad56b2de1100ff1d3c0c9d9d210
Robertspoon <robertthemcjug@gmail.com>
2022-09-11 23:02:08 -0300

    Added powerup and more to second floor

---
+++ Assets/scripts/powerup.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerup : MonoBehaviour
+{
+     
+     public float multiplier = 1.4f; 
+
+     public GameObject pickupEffect;
+
+
+     void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup(other);
+        }
+    }  
+
+    void Pickup(Collider player)
+    {
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        player.transform.localScale *= multiplier;
+
+        Destroy(gameObject);
+    }
+
+}

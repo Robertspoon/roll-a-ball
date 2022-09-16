9c0bd952718919865f3da2cb293b5c052b4c089d
Robertspoon <robertthemcjug@gmail.com>
2022-09-16 18:20:39 -0300

    small tweaks, began third level

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -40,7 +40,7 @@ public class playercontroller : MonoBehaviour
        void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if(count >= 12)
+        if(count >= 13)
         {
             winTextObject.SetActive(true);
         }

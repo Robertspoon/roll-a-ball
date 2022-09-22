f3b41faf24907a69145ad05e45600bbbb8cc8e34
Robertspoon <robertthemcjug@gmail.com>
2022-09-22 15:06:43 -0300

    Added duration for powerups

--- Assets/scripts/powerup.cs
+++ Assets/scripts/powerup.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class powerup : MonoBehaviour
 {
      
-     public float multiplier = 1.4f; 
+     public float multiplier = 1.4f;
+     public float duration = 4f;
 
      public GameObject pickupEffect;
 
@@ -14,16 +15,22 @@ public class powerup : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            Pickup(other);
+           StartCoroutine (Pickup(other));
         }
     }  
 
-    void Pickup(Collider player)
+    IEnumerator Pickup(Collider player)
     {
         Instantiate(pickupEffect, transform.position, transform.rotation);
 
         player.transform.localScale *= multiplier;
 
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+
+        yield return new WaitForSeconds(duration);
+
+        player.transform.localScale /= multiplier;
         Destroy(gameObject);
     }
 

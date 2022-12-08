ee69aa967e566e4ff07824d580d2281113aafb99
Robertspoon <robertthemcjug@gmail.com>
2022-12-08 15:33:18 -0400

    Re added jumppad after having to revert

--- Assets/scripts/JumpPoweUp.cs
+++ Assets/scripts/JumpPoweUp.cs
@@ -4,43 +4,25 @@ using UnityEngine;
 
 public class JumpPoweUp : MonoBehaviour
 {
-    
-    public float duration = 4f;
-    public float durationTillRespawn = 5.0f;
-    //AudioSource myAudio;
 
-    public GameObject pickupEffect;
+        public GameObject player;
+        Vector3 direction;
+        public float jumpForce = 5f;
 
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
+        void Update()
         {
-            
-            StartCoroutine(Pickup(other));
+            //generate vector in the direction of jump pad's y axis multiplied with a factor jumpForce
+            direction = transform.TransformDirection(Vector3.up * jumpForce);
         }
-    }
 
-    IEnumerator Pickup(Collider player)
-    {
-       /* Instantiate(pickupEffect, transform.position, transform.rotation);
-
-        player.transform.localScale *= multiplier;*/
-
-        GetComponent<MeshRenderer>().enabled = false;
-        GetComponent<Collider>().enabled = false;
-
-        yield return new WaitForSeconds(duration);
-
-        GetComponent<MeshRenderer>().enabled = true;
-        GetComponent<Collider>().enabled = true;
-        yield return new WaitForSeconds(durationTillRespawn);
-
-        //player.transform.localScale /= multiplier;
-
-    }
-    /*void playAudio()
-    {
-        myAudio.Play();
-    }*/
+        void onCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("player"))
+            {
+                player = collision.gameObject;
+                //apply force to the Player's rigidbody to let him "jump"
+                player.GetComponent<Rigidbody>().AddForce(direction, ForceMode.Impulse);
+            }
+        }
 }
